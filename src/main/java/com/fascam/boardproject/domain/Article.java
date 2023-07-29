097b0cc67c16cfc9b506cd1fb8c9352b75c50b00
un <mapzine123@naver.com>
2023-07-30 04:11:54 +0900

    #8 - 도메인 코드를 엔티티로 변환
    도메인 클래스와 테이블을 1:1 매칭되도록 설계함
    생성자와 equals(), hashcode() 등이 도메인 맥락에 맞게끔 디테일하게 설정함

--- src/main/java/com/fascam/boardproject/domain/Article.java
+++ src/main/java/com/fascam/boardproject/domain/Article.java
@@ -7,10 +7,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter // Lombok, 모든 멤버변수 Getter 설정
 @ToString // 쉽게 출력 가능
@@ -20,12 +23,14 @@ import java.util.Objects;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
+// EntityListeners : 엔티티의 변화를 감지해 엔티티와 매핑된 테이블 데이터를 조작, AuditingListener는 엔티티의 저장, 수정을 감지
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Article {
     @Id // PK 설정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AutoIncrement, mysql은 IDENTITY 방식으로 autoIncrement해서 바꿔줘야함
     private Long id;
-    
+
     @Setter
     @Column(nullable = false) // NotNull, true가 기본값, 생략 가능
     private String title; // 제목
@@ -37,9 +42,19 @@ public class Article {
     @Setter
     private String hashtag; // 해시태그
 
+    /* 양방향 바인딩
+     *  실무에서는 강한 결합이 되기 때문에 안하는 경우도 있음.
+     * @OneToMany : 1 : N 매핑
+     */
+    @ToString.Exclude // 순환 참조의 연쇄 해제 ( Article ToString 확인 > Set<ArticleComment 발견 > ArticleComment ToString 확인 > Article 발견 > Article ToString 확인...
+    @OrderBy("id") // 정렬 기준 : id
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL) // mappedBy를 안하면 article_article_comment 이런식으로 두 엔티티를 더한 이름이 생김
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>(); // 중복을 허용하지 않고 여기서 보겠음
+
+    /* 메타 데이터 */
     @CreatedDate // 자동으로 생성일시 넣어줌
     @Column(nullable = false)
-    private LocalDateTime createAt; // 생성일시
+    private LocalDateTime createdAt; // 생성일시
 
     @CreatedBy // 자동으로 생성자 넣어줌 >> 누가 생성했는지는 어떻게 알지? >> JpaConfig에서 설정
     @Column(nullable = false, length = 100)
@@ -71,15 +86,14 @@ public class Article {
 
 
     /*
-    * 1. db 내의 entity는 pk가 같으면 같다고 볼 수 있다.
-    * 2. 만약 pk값이 null이면 (아직 영속화되지 않았다면) 동등성 검사의 의미가 없다고 간주
-    */
+     * 1. db 내의 entity는 pk가 같으면 같다고 볼 수 있다.
+     * 2. 만약 pk값이 null이면 (아직 영속화되지 않았다면) 동등성 검사의 의미가 없다고 간주
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Article)) return false;
-        Article article = (Article) o;
-        return id != null && Objects.equals(id, article.id);
+        if (!(o instanceof Article that)) return false;
+        return id != null && id.equals(that.id);
     }
 
     @Override
@@ -89,10 +103,10 @@ public class Article {
 }
 
 /*
-* Setter를 전체로 안거는 이유?
-* - id나 생성일시 등 시스템에서 자동으로 세팅해주는 부분에 대해서는 변경 가능성을 주고 싶지 않아서
-*
-* pattern matching?
-* java 14 버전 이후에 equals 시 Atricle article = (Article) o; 부분은 필요없는 코드로 보고 보완함
-* */
+ * Setter를 전체로 안거는 이유?
+ * - id나 생성일시 등 시스템에서 자동으로 세팅해주는 부분에 대해서는 변경 가능성을 주고 싶지 않아서
+ *
+ * pattern matching?
+ * java 14 버전 이후에 equals 시 Atricle article = (Article) o; 부분은 필요없는 코드로 보고 보완함
+ * */
 
