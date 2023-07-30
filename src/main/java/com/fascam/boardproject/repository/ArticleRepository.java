a6045bce19ed953fe609d7d13e810bc08f989bc4
un <mapzine123@naver.com>
2023-07-31 02:31:33 +0900

    #1 - data rest 설정
    게시글, 댓글의 json api를 자동으로 
    restful 하게 만들게끔 설정

--- src/main/java/com/fascam/boardproject/repository/ArticleRepository.java
+++ src/main/java/com/fascam/boardproject/repository/ArticleRepository.java
@@ -2,9 +2,11 @@ package com.fascam.boardproject.repository;
 
 import com.fascam.boardproject.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /*
 * ctrl + shift + t : TDD 만들기
 */
+@RepositoryRestResource // Repository를 Rest Api로 노출 시킬건지
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
