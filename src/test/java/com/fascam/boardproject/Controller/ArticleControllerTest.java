b6933dbba3401056c9d6450653a5b28eaadc7af8
un <mapzine123@naver.com>
2023-07-31 23:31:29 +0900

    #14 - 게시글 뷰 테스트 내용 업데이트
    테스트가 실패하는 코드를 `grable build`하면 빌드가 되지 않아 배포 자동화에 악영향을 미치는 경우가 있으므로, 실패 테스트는 테스트 제외 처리하는 방향으로 정책을 잡아보기로 함.
    이에 기능 구현중인 유닛 테스트를 모두 `@Disabled` 처리
    
    미디어 타입 검사방법을 8b8acb4 에서 확인했기 때문에, 나머지 뷰도 동일하게 작동할 것으로 예상하고 미리 테스트 메소드 업데이트

--- src/test/java/com/fascam/boardproject/Controller/ArticleControllerTest.java
+++ src/test/java/com/fascam/boardproject/Controller/ArticleControllerTest.java
@@ -1,6 +1,7 @@
 package com.fascam.boardproject.Controller;
 
 import com.fascam.boardproject.controller.ArticleController;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 
 @DisplayName("View Controller - Article")
 @WebMvcTest(ArticleController.class) // 모든 컨트롤러에 대한 테스트가 아니라 ArticleController.class 명시 > ArticleController에 대해서만 테스트 실행
@@ -25,6 +29,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
+    @Disabled("구현 중") // 단위 메서드별로 테스트 제외 어노테이션
     @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnsArticleView() throws Exception {
@@ -32,11 +37,12 @@ class ArticleControllerTest {
 
         // When & Then
         mvc.perform(get("/articles"))
-                .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().contentType(MediaType.TEXT_HTML))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
+                .andExpect(status().isOk()) // 서버 통신 ok?
+                .andExpect((ResultMatcher) content().contentType(MediaType.TEXT_HTML)) // view 니까 TEXT_HTML 형식의 content가 있는지
+                .andExpect(view().name("articles/index")) // HTML 파일 이름 resolving
+                .andExpect(model().attributeExists("articles")); // model로 넘어오는 속성 이름
     }
-
+    @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
@@ -46,9 +52,11 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect((ResultMatcher) content().contentType(MediaType.TEXT_HTML))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("articleComment"));
     }
-
+    @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
@@ -57,9 +65,10 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().contentType(MediaType.TEXT_HTML));
+                .andExpect((ResultMatcher) content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));
     }
-
+    @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
@@ -68,6 +77,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().contentType(MediaType.TEXT_HTML));
+                .andExpect((ResultMatcher) content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists(("articles/search-hashtag")));
     }
 }
